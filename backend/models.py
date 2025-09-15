00b6d034e1f185a810322f3ec2f19e46849f6dcf
Sharguidev <guillermoj.obando@gmail.com>
2025-09-15 16:02:25 +0000

    Docker confirguration Completed

--- backend/models.py
+++ backend/models.py
@@ -5,6 +5,7 @@ from sqlalchemy.orm import Mapped, mapped_column
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__: 'user'
     id: Mapped[int] = mapped_column(primary_key=True)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
